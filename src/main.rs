41585dd34ca78867262b912db78829923063b75a
Vsevolod Velichko <torkvemada@sorokdva.net>
2017-12-08 20:11:31 +0300

    list photos

--- src/main.rs
+++ src/main.rs
@@ -64,8 +64,27 @@ fn make_dirattr(inode: u64, ts: Timespec) -> FileAttr {
         rdev: 0,
         flags: 0,
     }
-
 }
+
+fn make_fileattr(inode: u64, filesize: u64, ts: Timespec) -> FileAttr {
+    FileAttr {
+        ino: inode,
+        size: filesize,
+        blocks: 0,
+        atime: ts,
+        mtime: ts,
+        ctime: ts,
+        crtime: ts,
+        kind: FileType::RegularFile,
+        perm: 0o555,
+        nlink: 1,
+        uid: 0,
+        gid: 0,
+        rdev: 0,
+        flags: 0,
+    }
+}
+
 struct ShotwellVFS;
 
 impl ShotwellVFS {
@@ -78,7 +97,21 @@ impl ShotwellVFS {
             .and_then(|x| x.find(']').and_then(|end| x[1..end].parse::<u64>().ok()))
     }
 
-    fn fill_events(&mut self, mut reply: fuse::ReplyDirectory, offset: i64) {
+    fn readdir_root(&mut self, mut reply: fuse::ReplyDirectory, offset: i64) {
+        if offset != 0 {
+            reply.error(ENOENT);
+        } else {
+            reply.add(ROOT, 0, FileType::Directory, ".");
+            reply.add(ROOT, 1, FileType::Directory, "..");
+            reply.add(PHOTO, 2, FileType::Directory, "photos");
+            reply.add(VIDEO, 3, FileType::Directory, "videos");
+            reply.add(TAG, 4, FileType::Directory, "tags");
+            reply.add(EVENT, 5, FileType::Directory, "events");
+            reply.ok()
+        }
+    }
+
+    fn readdir_events(&mut self, mut reply: fuse::ReplyDirectory, offset: i64) {
         if offset < 0 {
             reply.error(ENOENT);
             return;
@@ -110,6 +143,81 @@ impl ShotwellVFS {
         };
         reply.ok();
     }
+
+    fn readdir_photos(&mut self, mut reply: fuse::ReplyDirectory, offset: i64) {
+        if offset < 0 {
+            reply.error(ENOENT);
+            return;
+        }
+        if offset == 0 {
+            reply.add(PHOTO, 0, FileType::Directory, ".");
+            reply.add(ROOT, 1, FileType::Directory, "..");
+        }
+        let mut idx = offset + 2;
+        let connection = sqlite::open("/home/torkve/.local/share/shotwell/data/photo.db").unwrap();
+        let mut statement = connection.prepare("SELECT id, filename, timestamp, title FROM PhotoTable ORDER BY timestamp ASC LIMIT ?, 100").unwrap();
+        statement.bind(1, offset).unwrap();
+        while let Ok(sqlite::State::Row) = statement.next() {
+            let photo_id = statement.read::<i64>(0).unwrap() as u64;
+            let inode = photo_id | PHOTO;
+            let filename = statement.read::<Vec<u8>>(1).map_err(|_|()).and_then(|x| String::from_utf8(x).map_err(|_|())).unwrap_or(String::new());
+            let extension = filename.rfind('.').map(|x| &filename[x+1..]).unwrap_or("");
+            let title = statement.read::<Vec<u8>>(3).map_err(|_|()).and_then(|x| String::from_utf8(x).map_err(|_|())).unwrap_or(String::new());
+            if !title.is_empty() {
+                debug!("photo id {} has utf name {:?}", photo_id, title);
+                reply.add(inode, idx, FileType::RegularFile, format!("[{}] {}.{}", photo_id, title, extension));
+                idx += 1;
+            } else {
+                let timestamp = time::at(time::Timespec{sec: statement.read::<i64>(2).unwrap(), nsec: 0});
+                let tm = timestamp.strftime("%Y-%m-%d %H:%M").unwrap();
+                debug!("photo id {} title is empty, using timestamp `{}`", photo_id, tm);
+                reply.add(inode, idx, FileType::RegularFile, format!("[{}] {}.{}", photo_id, tm, extension));
+                idx += 1;
+            }
+        };
+        reply.ok();
+    }
+
+    fn lookup_root(&mut self, name: &OsStr, reply: ReplyEntry) {
+        match name.to_str() {
+            Some("/") => reply.entry(&TTL, &ROOT_ATTR, 0),
+            Some("photos") => reply.entry(&TTL, &PHOTO_ATTR, 0),
+            Some("videos") => reply.entry(&TTL, &VIDEO_ATTR, 0),
+            Some("tags") => reply.entry(&TTL, &TAG_ATTR, 0),
+            Some("events") => reply.entry(&TTL, &EVENT_ATTR, 0),
+            _ => reply.error(ENOENT),
+        }
+    }
+
+    fn lookup_event(&mut self, name: &OsStr, reply: ReplyEntry) {
+        if let Some(id) = self.extract_id(name) {
+            let connection = sqlite::open("/home/torkve/.local/share/shotwell/data/photo.db").unwrap();
+            let mut statement = connection.prepare("SELECT time_created FROM EventTable WHERE id = ?").unwrap();
+            statement.bind(1, id as i64).unwrap();
+            if let Ok(sqlite::State::Row) = statement.next() {
+                let timestamp = time::Timespec{sec: statement.read::<i64>(0).unwrap(), nsec: 0};
+                reply.entry(&TTL, &make_dirattr(EVENT | id, timestamp), 0);
+                return;
+            }
+        }
+        reply.error(ENOENT);
+    }
+
+    fn lookup_photo(&mut self, name: &OsStr, reply: ReplyEntry) {
+        if let Some(id) = self.extract_id(name) {
+            let connection = sqlite::open("/home/torkve/.local/share/shotwell/data/photo.db").unwrap();
+            let mut statement = connection.prepare("SELECT filesize, timestamp FROM PhotoTable WHERE id = ?").unwrap();
+            statement.bind(1, id as i64).unwrap();
+            if let Ok(sqlite::State::Row) = statement.next() {
+                let timestamp = time::Timespec{sec: statement.read::<i64>(1).unwrap(), nsec: 0};
+                let filesize = statement.read::<i64>(0).unwrap() as u64;
+                reply.entry(&TTL, &make_fileattr(EVENT | id, filesize, timestamp), 0);
+                return;
+            }
+        }
+        reply.error(ENOENT);
+
+    }
 }
 
 impl Filesystem for ShotwellVFS {
@@ -120,29 +228,9 @@ impl Filesystem for ShotwellVFS {
               reply: ReplyEntry,
               ) {
         match parent {
-            ROOT => match name.to_str() {
-                Some("/") => reply.entry(&TTL, &ROOT_ATTR, 0),
-                Some("photos") => reply.entry(&TTL, &PHOTO_ATTR, 0),
-                Some("videos") => reply.entry(&TTL, &VIDEO_ATTR, 0),
-                Some("tags") => reply.entry(&TTL, &TAG_ATTR, 0),
-                Some("events") => reply.entry(&TTL, &EVENT_ATTR, 0),
-                _ => reply.error(ENOENT),
-            },
-            EVENT => {
-                if let Some(id) = self.extract_id(name) {
-                    let connection = sqlite::open("/home/torkve/.local/share/shotwell/data/photo.db").unwrap();
-                    let mut statement = connection.prepare("SELECT time_created FROM EventTable WHERE id = ?").unwrap();
-                    statement.bind(1, id as i64).unwrap();
-                    if let Ok(sqlite::State::Row) = statement.next() {
-                        let timestamp = time::Timespec{sec: statement.read::<i64>(0).unwrap(), nsec: 0};
-                        reply.entry(&TTL, &make_dirattr(EVENT | id, timestamp), 0);
-                    } else {
-                        reply.error(ENOENT);
-                    }
-                } else {
-                    reply.error(ENOENT);
-                }
-            }
+            ROOT => self.lookup_root(name, reply),
+            EVENT => self.lookup_event(name, reply),
+            PHOTO => self.lookup_photo(name, reply),
             _ => reply.error(ENOENT),
         };
     }
@@ -170,21 +258,16 @@ impl Filesystem for ShotwellVFS {
                mut reply: fuse::ReplyDirectory,
                ) {
         if inode == EVENT {
-            self.fill_events(reply, offset);
+            self.readdir_events(reply, offset);
+        }
+        else if inode == PHOTO {
+            self.readdir_photos(reply, offset);
         }
         else if offset != 0 {
             reply.error(ENOENT);
         } else {
             match inode {
-                ROOT => {
-                    reply.add(ROOT, 0, FileType::Directory, ".");
-                    reply.add(ROOT, 1, FileType::Directory, "..");
-                    reply.add(PHOTO, 2, FileType::Directory, "photos");
-                    reply.add(VIDEO, 3, FileType::Directory, "videos");
-                    reply.add(TAG, 4, FileType::Directory, "tags");
-                    reply.add(EVENT, 5, FileType::Directory, "events");
-                    reply.ok()
-                },
+                ROOT => self.readdir_root(reply, offset),
                 PHOTO => {
                     reply.add(PHOTO, 0, FileType::Directory, ".");
                     reply.add(ROOT, 1, FileType::Directory, "..");
@@ -200,7 +283,7 @@ impl Filesystem for ShotwellVFS {
                     reply.add(ROOT, 1, FileType::Directory, "..");
                     reply.ok()
                 },
-                EVENT => self.fill_events(reply, offset),
+                EVENT => self.readdir_events(reply, offset),
                 _ => reply.error(ENOENT)
             }
         };
