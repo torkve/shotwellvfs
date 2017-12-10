3f783f64da2c7ed7e3c334e10b009bc9e863c635
Vsevolod Velichko <torkvemada@sorokdva.net>
2017-12-10 19:57:33 +0300

    add args parser

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate fuse;
 extern crate sqlite;
 #[macro_use] extern crate log;
 extern crate env_logger;
+extern crate clap;
 
 use std::path::Path;
 use std::ffi::OsStr;
@@ -454,6 +455,31 @@ impl Filesystem for ShotwellVFS {
 
 fn main() {
     env_logger::init().unwrap();
-    let mountpoint = std::env::args_os().nth(1).unwrap();
-    fuse::mount(ShotwellVFS::new("/home/torkve/.local/share/shotwell/data/photo.db"), &mountpoint, &[]).unwrap();
+    let args = clap::App::new("Shotwell VFS")
+        .version("0.1")
+        .author("Vsevolod Velichko <torkvemada@sorokdva.net")
+        .about("Expose shotwell library as filesystem hierarchy")
+        .arg(clap::Arg::with_name("db")
+             .long("db")
+             .value_name("FILE")
+             .help("Custom path to database file")
+             .takes_value(true)
+            )
+        .arg(clap::Arg::with_name("MOUNTPOINT")
+             .help("Path to mount FS")
+             .required(true)
+             .index(1)
+             )
+        .get_matches();
+
+    let mountpoint = args.value_of("MOUNTPOINT").unwrap();
+    let vfs = match args.value_of("db") {
+        None => {
+            let mut path = std::env::home_dir().unwrap_or_else(|| panic!("Cannot find user home dir and no --db argument specified"));
+            path.push(".local/share/shotwell/data/photo.db");
+            ShotwellVFS::new(path)
+        },
+        Some(path) => ShotwellVFS::new(path),
+    };
+    fuse::mount(vfs, &mountpoint, &[]).unwrap();
 }
